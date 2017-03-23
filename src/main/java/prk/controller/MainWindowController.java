45b285bce77716521a7a1da0e53508e742cfae3f
wkrzywiec <wojciechkrzywiec@gmail.com>
2017-03-23 01:18:00 +0100

    Pierwsze klasy gry
    Stworzone zostały najbardziej podstawowe dwie klasy gry, tak aby móc
    uruchomić okienko z grą. Dodatkowo został tło gry zostało przeniesione
    do folderu prk.view. W końcu wszystkie textfieldy zostały dodane

---
+++ src/main/java/prk/controller/MainWindowController.java
@@ -0,0 +1,16 @@
+package prk.controller;
+
+import javafx.stage.Stage;
+
+public class MainWindowController {
+
+	private Stage primaryStage;
+	
+	public void setStage(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+	}
+
+	public void initialize() {
+		
+	}
+}
